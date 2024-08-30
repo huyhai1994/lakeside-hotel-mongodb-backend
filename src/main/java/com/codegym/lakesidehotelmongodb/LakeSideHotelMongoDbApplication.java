f16c38097b5ca6b09d3fec87af786c7b10338fdb
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-30 08:16:42 +0700

    init project

---
+++ src/main/java/com/codegym/lakesidehotelmongodb/LakeSideHotelMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.lakesidehotelmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeSideHotelMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakeSideHotelMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/lakesidehotelmongodb/LakeSideHotelMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.lakesidehotelmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideHotelMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
