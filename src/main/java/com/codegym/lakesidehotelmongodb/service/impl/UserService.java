d0b455f8875d05af1b84861badeb8b2d20743968
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-09-02 13:24:07 +0700

    update UserService.java

--- src/main/java/com/codegym/lakesidehotelmongodb/service/impl/UserService.java
+++ src/main/java/com/codegym/lakesidehotelmongodb/service/impl/UserService.java
@@ -2,18 +2,81 @@ package com.codegym.lakesidehotelmongodb.service.impl;
 
 import com.codegym.lakesidehotelmongodb.dto.LoginRequest;
 import com.codegym.lakesidehotelmongodb.dto.Response;
+import com.codegym.lakesidehotelmongodb.dto.UserDTO;
 import com.codegym.lakesidehotelmongodb.entity.User;
+import com.codegym.lakesidehotelmongodb.exception.OurException;
+import com.codegym.lakesidehotelmongodb.repo.UserRepository;
 import com.codegym.lakesidehotelmongodb.service.interfac.IUserService;
+import com.codegym.lakesidehotelmongodb.utils.JWTUtils;
+import com.codegym.lakesidehotelmongodb.utils.Utils;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Service
 public class UserService implements IUserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public Response register(User user) {
-        return null;
+        Response response = new Response();
+        try {
+            if (user.getRole() == null || user.getRole().isBlank()) {
+                user.setRole("USER");
+            }
+            if (userRepository.existsByEmail(user.getId())) {
+                throw new OurException("Email already exists");
+            }
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User savedUser = userRepository.save(user);
+
+            UserDTO userDTO = Utils.mapUserEntityToUserDTO(savedUser);
+            response.setStatusCode(200);
+            response.setMessage("User registered successfully");
+            response.setUser(userDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error while registering user" + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response login(LoginRequest loginRequest) {
-        return null;
+        Response response = new Response();
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getEmail(),
+                            loginRequest.getPassword()));
+            var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(() -> new OurException("User not found"));
+            var token = jwtUtils.generateToken(user);
+            response.setStatusCode(200);
+            response.setToken(token);
+            response.setRole(user.getRole());
+            response.setExpirationTime("7 days");
+
+
+        } catch (OurException e) {
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error while login in a user" + e.getMessage());
+        }
+        return response;
     }
 
     @Override
