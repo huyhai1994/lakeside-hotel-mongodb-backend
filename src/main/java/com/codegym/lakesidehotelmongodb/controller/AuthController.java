c31917c96488dd67e1bd1f3d0ba3e69213bd64ed
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-09-02 20:06:56 +0700

    test sign in / sign up by Postman ok

---
+++ src/main/java/com/codegym/lakesidehotelmongodb/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.codegym.lakesidehotelmongodb.controller;
+
+import com.codegym.lakesidehotelmongodb.dto.LoginRequest;
+import com.codegym.lakesidehotelmongodb.dto.Response;
+import com.codegym.lakesidehotelmongodb.entity.User;
+import com.codegym.lakesidehotelmongodb.service.interfac.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
